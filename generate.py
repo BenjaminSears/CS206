1843eb9fdf4aae6e348b59335bb2da7340a972fd
BenjaminSears <44883042+BenjaminSears@users.noreply.github.com>
2021-02-10 17:46:28 -0500

    One torso, two legged robot

--- generate.py
+++ generate.py
@@ -3,17 +3,29 @@ length = 1
 width = 1
 height = 1
 
-def Create_World(x,y,z):
+
+def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+    # pyrosim.Send_Cube(name="Box", pos=[-3,3,.5], size=[length, width, height])
     pyrosim.End()
 
 
-def Create_Robot(x,y,z):
+def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[x, y, z], size=[length, width, height])
+    # pyrosim.Send_Cube(name="Torso", pos=[0, 0, .5], size=[length, width, height])
+    # pyrosim.Send_Joint(name="Torso_Leg", parent="Torso", child="Leg", type="revolute", position="0.5, 0.0, 1.0")
+    # pyrosim.Send_Cube(name="Leg", pos=[0.5, 0.0, 0.5], size=[length, width, height])
+
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[length, width, height])
+
+    pyrosim.Send_Joint(name="Torso_Front_Leg", parent="Torso", child="Front_Leg", type="revolute", position="2.0, 0.0, 1.0")
+    pyrosim.Send_Cube(name="Front_Leg", pos=[.5, 0, -.5], size=[length, width, height])
+
+    pyrosim.Send_Joint(name="Torso_Back_Leg", parent="Torso", child="Back_Leg", type="revolute", position="1.0, 0.0, 1.0")
+    pyrosim.Send_Cube(name="Back_Leg", pos=[-.5, 0, -.5], size=[length, width, height])
+
     pyrosim.End()
 
 
-Create_World(-3,3,.5)
-Create_Robot(0,0,.5)
+Create_World()
+Create_Robot()
