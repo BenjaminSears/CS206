dd939ba8f35506013af2d2de4aea129a2f8df7f9
BenjaminSears <44883042+BenjaminSears@users.noreply.github.com>
2021-02-16 12:44:08 -0500

    Plot both sensors

--- analyze.py
+++ analyze.py
@@ -2,7 +2,10 @@ import numpy as np
 import matplotlib.pyplot as mp
 
 backLegSensorValues = np.load("data/backLegSensorValues.npy")
-mp.plot(backLegSensorValues)
+frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
+mp.plot(backLegSensorValues, label="Back", linewidth=3)
+mp.plot(frontLegSensorValues, label="Front")
+mp.legend()
 mp.show()
 
 
--- simulate.py
+++ simulate.py
@@ -5,19 +5,22 @@ import pyrosim.pyrosim as pyrosim
 import numpy as np
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-
-
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate("body.urdf")
+
 backLegSensorValues = np.zeros(100)
+frontLegSensorValues = np.zeros(100)
+
 for i in range(100):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Back_Leg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Front_Leg")
     t.sleep(1/20)
 
 
 np.save("data/backLegSensorValues.npy", backLegSensorValues)
+np.save("data/frontLegSensorValues.npy", frontLegSensorValues)
 p.disconnect()
