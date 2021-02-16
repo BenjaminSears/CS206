2f17e66f2bc9535cd92c43b8ae761f5702f53c41
BenjaminSears <44883042+BenjaminSears@users.noreply.github.com>
2021-02-16 12:17:04 -0500

    Saved back leg sensor values to a vector

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import pybullet_data
 import time as t
+import pyrosim.pyrosim as pyrosim
+import numpy as np
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
@@ -9,8 +11,12 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
-for i in range(1000):
+pyrosim.Prepare_To_Simulate("body.urdf")
+backLegSensorValues = np.zeros(100)
+for i in range(100):
     p.stepSimulation()
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Back_Leg")
     t.sleep(1/20)
 
+print(backLegSensorValues)
 p.disconnect()
