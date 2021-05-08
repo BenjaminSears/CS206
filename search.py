9ca2c05d85142b9cd05d061a9f43b659d3f7def0
BenjaminSears <44883042+BenjaminSears@users.noreply.github.com>
2021-05-08 14:40:01 -0400

    Alpha write both files out

--- search.py
+++ search.py
@@ -4,7 +4,8 @@ import numpy as np
 phc = PARALLEL_HILL_CLIMBER()
 phc.Evolve()
 phc.Show_Best()
-np.savetxt("C:/Users/benja/Desktop/CS206/Projects/Hillclimber/data/A_Data.txt", c.data, fmt=('%10.3f'))
+np.savetxt("C:/Users/benja/Desktop/CS206/Projects/Hillclimber/data/Alpha_Data.txt", c.data, fmt=('%10.3f'))
+np.save("C:/Users/benja/Desktop/CS206/Projects/Hillclimber/data/Alpha_Data_NP", c.data)
 # for i in range(5):
 #     os.system("python3 generate.py")
 #     os.system("python3 simulate.py")
