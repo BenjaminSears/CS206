b800fa98df906139fdf20c8386818e88c8b18b91
BenjaminSears <44883042+BenjaminSears@users.noreply.github.com>
2021-05-08 14:41:01 -0400

    Beta modify 3 weights and write out both file formats

--- search.py
+++ search.py
@@ -5,7 +5,7 @@ phc = PARALLEL_HILL_CLIMBER()
 phc.Evolve()
 phc.Show_Best()
 np.savetxt("C:/Users/benja/Desktop/CS206/Projects/Hillclimber/data/Beta_Data.txt", c.data, fmt=('%10.3f'))
-np.savetxt("C:/Users/benja/Desktop/CS206/Projects/Hillclimber/data/Beta_Data_NP", c.data, fmt=('%10.3f'))
+np.save("C:/Users/benja/Desktop/CS206/Projects/Hillclimber/data/Beta_Data_NP", c.data)
 # for i in range(5):
 #     os.system("python3 generate.py")
 #     os.system("python3 simulate.py")
